6871fc37c37a95c925f067d33f40a0216dcde948
JuanAguirreCastro <juanc.aguirre@pascualbravo.edu.co>
2025-09-22 16:10:40 -0500

    Descripción del cambioProceso 1: Mutación del estado y reacción ante cambios de estado

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -19,7 +19,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.ElevatedButton
-
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,27 +40,32 @@ fun MyApp(
     names: List<String> = listOf("World", "Compose")
 ) {
     Column(modifier) {
-        for (name in names){
+        for (name in names) {
             Greeting(name = name)
         }
-
     }
 }
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    // Estado para controlar el texto del botón
+    val expanded = remember { mutableStateOf(false) }
+
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f)){
+            Column(modifier = Modifier.weight(1f)) {
                 Text(text = "Hello")
                 Text(text = name)
             }
+
+            // Botón que solo cambia el texto (sin expandir nada)
             ElevatedButton(
-                onClick =  {/* TODO */}
+                onClick = { expanded.value = !expanded.value }
             ) {
-                Text("Show More")
+                Text(if (expanded.value) "Show less" else "Show more")
             }
         }
     }
